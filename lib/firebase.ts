377ff832955577cb11425eb9640c46f34230ec79
Alex0White <alexjwhitex@gmail.com>
2022-03-08 21:05:43 +1300

    heartbutton not working

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,6 +5,10 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
 
 
+export const fromMillis = firebase.firestore.Timestamp.fromMillis;
+
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyAOUZ-ERkOltSC8_9vCyMElBeMHrkwef0Q",
   authDomain: "nextfire-c9b14.firebaseapp.com",
@@ -19,13 +23,37 @@ if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
 }
 
+/**`
+ * Gets a users/{uid} document with username
+ * @param  {string} username
+ */
+ export async function getUserWithUsername(username) {
+  const usersRef = firestore.collection('users');
+  const query = usersRef.where('username', '==', username).limit(1);
+  const userDoc = (await query.get()).docs[0];
+  return userDoc;
+}
+
+/**`
+ * Converts a firestore document to JSON
+ * @param  {DocumentSnapshot} doc
+ */
+export function postToJSON(doc) {
+  const data = doc.data();
+  return {
+    ...data,
+    // Gotcha! firestore timestamp NOT serializable to JSON. Must convert to milliseconds
+    createdAt: data.createdAt.toMillis(),
+    updatedAt: data.updatedAt.toMillis(),
+  };
+}
+
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 export const storage = firebase.storage();
 export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+export const serverTimestamp = firebase.firestore.FieldValue.serverTimestamp;
 
-
-
-
-
-
+export const STATE_CHANGED = firebase.storage.TaskEvent.STATE_CHANGED;
+export const increment = firebase.firestore.FieldValue.increment;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,65 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Link from 'next/link'
+import PostFeed from '../components/PostFeed';
 import Loader from '../components/Loader';
-import { Component } from 'react';
-import toast from 'react-hot-toast';
+import { firestore, fromMillis, postToJSON } from '../lib/firebase';
 
-export default function Home() {
-  return (
-    <div>
-      <Loader show/>
-      <button onClick={() => toast.success('hello toast!')}>
-      Toast Me
+import { useState } from 'react';
 
-      </button>
-    </div>
-  )
+// Max post to query per page
+const LIMIT = 1;
+
+export async function getServerSideProps(context) {
+  const postsQuery = firestore
+    .collectionGroup('posts')
+    .where('published', '==', true)
+    .orderBy('createdAt', 'desc')
+    .limit(LIMIT);
+
+  const posts = (await postsQuery.get()).docs.map(postToJSON);
+
+  return {
+    props: { posts }, // will be passed to the page component as props
+  };
 }
 
+export default function Home(props) {
+  const [posts, setPosts] = useState(props.posts);
+  const [loading, setLoading] = useState(false);
+
+  const [postsEnd, setPostsEnd] = useState(false);
+
+  const getMorePosts = async () => {
+    setLoading(true);
+    const last = posts[posts.length - 1];
+
+    const cursor = typeof last.createdAt === 'number' ? fromMillis(last.createdAt) : last.createdAt;
+
+    const query = firestore
+      .collectionGroup('posts')
+      .where('published', '==', true)
+      .orderBy('createdAt', 'desc')
+      .startAfter(cursor)
+      .limit(LIMIT);
+
+    const newPosts = (await query.get()).docs.map((doc) => doc.data());
+
+    setPosts(posts.concat(newPosts));
+    setLoading(false);
+
+    if (newPosts.length < LIMIT) {
+      setPostsEnd(true);
+    }
+  };
+
+  return (
+      <main>
+        <PostFeed posts={posts} admin={undefined} />
+
+        {!loading && !postsEnd && <button onClick={getMorePosts}>Load more</button>}
+
+        <Loader show={loading} />
+
+        {postsEnd && 'You have reached the end!'}
+      </main>
+  );
+}
 
